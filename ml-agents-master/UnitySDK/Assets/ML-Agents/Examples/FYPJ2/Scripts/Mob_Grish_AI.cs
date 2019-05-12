a96f0625eebf67b335bc445949881ceec065e71f
DESKTOP-8MAC91V\Jav <javierng2knyp@gmail.com>
2019-05-12 16:54:47 +0800

    Start of repo

---
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/FYPJ2/Scripts/Mob_Grish_AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob_Grish_AI : MonoBehaviour {
+
+    public GameObject m_GrishGO;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_GrishGO.transform.Translate(1,0,0);
+	}
+}
